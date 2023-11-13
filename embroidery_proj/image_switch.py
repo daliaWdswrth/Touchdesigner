64c81193c3168fe67882e849866eb31033873500
dlw <daliawor@gmail.com>
2023-11-13 18:04:46 -0500

    changes to image switch

--- embroidery_proj/image_switch.py
+++ embroidery_proj/image_switch.py
@@ -3,12 +3,12 @@ import sys
 import random
 import numpy
 
-baseOP = op(Render_Particles)
 
 if op('keyboardin1')['k1'] == 1:
+	op('select1').par.top = "image1"
 
 if op('keyboardin1')['k2'] == 1:
+	op('select1').par.top = "image2"
 
 if op('keyboardin1')['k3'] == 1:
-
-if op('keyboardin1')['k4'] == 1:
+	op('select1').par.top = "image3"
